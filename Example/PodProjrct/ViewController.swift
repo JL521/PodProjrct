3f8e2e09bf1470eda25e2fc375dc86d75f709c3b
jiang <314886745@qq.com>
2020-03-10 10:23:18 +0800

    hh

--- Example/PodProjrct/ViewController.swift
+++ Example/PodProjrct/ViewController.swift
@@ -21,6 +21,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let titles = ["test1", "test2", "test3"]
+        let childs = [UIViewController(), UIViewController(), UIViewController()]
+        let pagevc = RCPagemenController(titles: titles, childs: childs, count: 3 )
+        pagevc.selectBlock = {
+            (index: Int) in
+            print("\(titles[index])")
+        }
+        self.present(pagevc, animated: true, completion: nil)
+    }
 
 }
 
